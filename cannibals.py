0d80aa3aee4fa6f28cf5bdbc34bda77f09f0dc4d
19kummitha <113335352+19kummitha@users.noreply.github.com>
2022-09-22 14:08:56 +0530

    Update cannibals.py
    |🌊🌊🌊🌊🌊🚢|💂💂💂👹👹👹
    Enter number of Missionary in boat on Right:1
    Enter number of Cannibals in boat on Right:1
    💂👹|🚢🌊🌊🌊🌊🌊|💂💂👹👹
    Enter number of Missionary in boat on Left:1
    Enter number of Cannibals in boat on Left:0
    👹|🌊🌊🌊🌊🌊🚢|💂💂💂👹👹
    Enter number of Missionary in boat on Right:0
    Enter number of Cannibals in boat on Right:2
    👹👹👹|🚢🌊🌊🌊🌊🌊|💂💂💂
    Enter number of Missionary in boat on Left:0
    Enter number of Cannibals in boat on Left:1
    👹👹|🌊🌊🌊🌊🌊🚢|💂💂💂👹
    Enter number of Missionary in boat on Right:2
    Enter number of Cannibals in boat on Right:0
    💂💂👹👹|🚢🌊🌊🌊🌊🌊|💂👹
    Enter number of Missionary in boat on Left:1
    Enter number of Cannibals in boat on Left:1
    💂👹|🌊🌊🌊🌊🌊🚢|💂💂👹👹
    Enter number of Missionary in boat on Right:2
    Enter number of Cannibals in boat on Right:0
    💂💂💂👹|🚢🌊🌊🌊🌊🌊|👹👹
    Enter number of Missionary in boat on Left:0
    Enter number of Cannibals in boat on Left:1
    💂💂💂|🌊🌊🌊🌊🌊🚢|👹👹👹
    Enter number of Missionary in boat on Right:0
    Enter number of Cannibals in boat on Right:2
    💂💂💂👹👹|🚢🌊🌊🌊🌊🌊|👹
    Enter number of Missionary in boat on Left:0
    Enter number of Cannibals in boat on Left:1
    💂💂💂👹|🌊🌊🌊🌊🌊🚢|👹👹
    Enter number of Missionary in boat on Right:0
    Enter number of Cannibals in boat on Right:2
    💂💂💂👹👹👹|🚢🌊🌊🌊🌊🌊|
    YOU WIN
    GAME OVER

--- cannibals.py
+++ cannibals.py
@@ -1,93 +1,95 @@
-print("\n")
-print("\tGame Start\nNow the task is to move all of them to right side of the river")
-print("rules:\n1. The boat can carry at most two people\n2. If cannibals num greater then missionaries then the cannibals would eat the missionaries\n3. The boat cannot cross the river by itself with no people on board")
-lM = 3   #lM = Left side Missionaries number
-lC = 3   #lC = Laft side Cannibals number
-rM=0   #rM = Right side Missionaries number
-rC=0   #rC = Right side cannibals number
-userM = 0  #userM = User input for number of missionaries for right to left side travel
-userC = 0  #userC = User input for number of cannibals for right to left travel
-k = 0
-print("\nM M M C C C |  --- | \n")
-try:
- while(True):
-  while(True):
-   print("Left side -> right side river travel")
-   #uM = user input for number of missionaries for left to right travel
-   #uC = user input for number of cannibals for left to right travel
-   uM = int(input("Enter number of Missionaries travel => "))
-   uC = int(input("Enter number of Cannibals travel => "))
+boat_side = "Right"
+missionary_on_right = 3
+cannibals_on_right = 3
+missionary_on_left = 0
+cannibals_on_left = 0
 
-   if((uM==0)and(uC==0)):
-    print("Empty travel not possible")
-    print("Re-enter : ")
-   elif(((uM+uC) <= 2)and((lM-uM)>=0)and((lC-uC)>=0)):
-    break
-   else:
-    print("Wrong input re-enter : ")
-  lM = (lM-uM)
-  lC = (lC-uC)
-  rM += uM
-  rC += uC
+user_interface = ''
+for i in range(0,missionary_on_left):
+    user_interface += "\U0001f482"
+for i in range(0,cannibals_on_left):
+    user_interface += "\U0001f479"
+user_interface += '|'
+for i in range(0,5):
+    user_interface += "\U0001f30a"
+user_interface += "\U0001f6A2|"
+for i in range(0,missionary_on_right):
+    user_interface += "\U0001f482"
+for i in range(0,cannibals_on_right):
+    user_interface += "\U0001f479"
+print(user_interface)
 
-  print("\n")
-  for i in range(0,lM):
-   print("M ",end="")
-  for i in range(0,lC):
-   print("C ",end="")
-  print("| --> | ",end="")
-  for i in range(0,rM):
-   print("M ",end="")
-  for i in range(0,rC):
-   print("C ",end="")
-  print("\n")
-
-  k +=1
-
-  if(((lC==3)and (lM == 1))or((lC==3)and(lM==2))or((lC==2)and(lM==1))or((rC==3)and (rM == 1))or((rC==3)and(rM==2))or((rC==2)and(rM==1))):
-   print("Cannibals eat missionaries:\nYou lost the game")
-
-   break
-
-  if((rM+rC) == 6):
-   print("You won the game : \n\tCongrats")
-   print("Total attempt")
-   print(k)
-   break
-  while(True):
-   print("Right side -> Left side river travel")
-   userM = int(input("Enter number of Missionaries travel => "))
-   userC = int(input("Enter number of Cannibals travel => "))
-   
-   if((userM==0)and(userC==0)):
-     print("Empty travel not possible")
-     print("Re-enter : ")
-   elif(((userM+userC) <= 2)and((rM-userM)>=0)and((rC-userC)>=0)):
-    break
-   else:
-    print("Wrong input re-enter : ")
-  lM += userM
-  lC += userC
-  rM -= userM
-  rC -= userC
-
-  k +=1
-  print("\n")
-  for i in range(0,lM):
-   print("M ",end="")
-  for i in range(0,lC):
-   print("C ",end="")
-  print("| <-- | ",end="")
-  for i in range(0,rM):
-   print("M ",end="")
-  for i in range(0,rC):
-   print("C ",end="")
-  print("\n")
-
- 
-
-  if(((lC==3)and (lM == 1))or((lC==3)and(lM==2))or((lC==2)and(lM==1))or((rC==3)and (rM == 1))or((rC==3)and(rM==2))or((rC==2)and(rM==1))):
-   print("Cannibals eat missionaries:\nYou lost the game")
-   break
-except EOFError as e:
- print("\nInvalid input please retry !!")
+while True:
+    missionary = int(input("Enter number of Missionary in boat on "+ boat_side + ":"))
+    cannibals = int(input("Enter number of Cannibals in boat on " + boat_side + ":"))
+    
+    #Maximum number of people one the boat should be 1 or 2
+    if (missionary+cannibals) != 1 and (missionary+cannibals) != 2:
+        print("Invalid move")
+        continue
+    
+    #Turn based decisions
+    if boat_side == "Right":
+        if missionary > missionary_on_right or cannibals > cannibals_on_right:
+            print("Invalid move")
+            continue
+           
+        missionary_on_right -= missionary
+        cannibals_on_right -= cannibals
+        missionary_on_left += missionary
+        cannibals_on_left += cannibals
+        
+        boat_side = "Left"
+        
+        user_interface = ''
+        for i in range(0,missionary_on_left):
+            user_interface += "\U0001f482"
+        for i in range(0,cannibals_on_left):
+            user_interface += "\U0001f479"
+        user_interface += '|\U0001f6A2'
+        for i in range(0,5):
+            user_interface += "\U0001f30a"
+        user_interface += "|"
+        for i in range(0,missionary_on_right):
+            user_interface += "\U0001f482"
+        for i in range(0,cannibals_on_right):
+            user_interface += "\U0001f479"
+        print(user_interface)
+        
+    else:
+        if missionary > missionary_on_left or cannibals > cannibals_on_left:
+            print("Invalid move")
+            continue
+            
+        missionary_on_right += missionary
+        cannibals_on_right += cannibals
+        missionary_on_left -= missionary
+        cannibals_on_left -= cannibals
+        
+        boat_side = "Right"
+        
+        user_interface = ''
+        for i in range(0,missionary_on_left):
+            user_interface += "\U0001f482"
+        for i in range(0,cannibals_on_left):
+            user_interface += "\U0001f479"
+        user_interface += '|'
+        for i in range(0,5):
+            user_interface += "\U0001f30a"
+        user_interface += "\U0001f6A2|"
+        for i in range(0,missionary_on_right):
+            user_interface += "\U0001f482"
+        for i in range(0,cannibals_on_right):
+            user_interface += "\U0001f479"
+        print(user_interface)
+     
+    #condition for losing
+    if (missionary_on_right != 0 and missionary_on_right < cannibals_on_right) or (missionary_on_left != 0 and missionary_on_left < cannibals_on_left):
+        print("YOU LOSE")
+        break
+    
+    #condition for winning
+    if missionary_on_left == 3 and cannibals_on_left == 3:
+        print("YOU WIN")
+        break
+print("GAME OVER")
